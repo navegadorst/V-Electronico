7a619ee29bc7af4cb1e186647c10fd3c0ea0f727
Alex Torres <alejandro.torres.e@gmail.com>
2019-09-18 02:02:47 -0500

    add packages with libraries for bootstrap, jquery and mvc update

--- ValeElectronico/Controllers/HomeController.cs
+++ ValeElectronico/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -24,6 +26,35 @@ namespace ValeElectronico.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            var client = new RestClient("https://YOUR_DOMAIN/api/v2/connections/CONNECTION_ID");
+            var request = new RestRequest(Method.PATCH);
+            request.AddHeader("authorization", "Bearer MGMT_API_ACCESS_TOKEN");
+            request.AddParameter("undefined", "{  \"options\": { \"identity-api\": \"microsoft-identity-platform-v2.0\", \"scopes\": \"email\" } }", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+
+
+
+            //for step 2
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                // Since this is a multi-tenant app, you should ideally only accept users from a list of tenants that you want to.
+                // * Instead of using the default validation (validating against a single issuer value, as we do in line of business apps), you can inject your own multi-tenant validation logic through a IssuerValidator.
+                // * Or you can provide a static list of acceptable tenantIds, as detailed below
+                // ValidIssuers = new List<string>()
+                // {
+                //     "https://sts.windows.net/6d9c0c36-c30e-442b-b60a-ca22d8994d14/",
+                //     "https://sts.windows.net/f69b5f46-9a0d-4a5c-9e25-54e42bbbd4c3/",
+                //     "https://sts.windows.net/fb674642-8965-493d-beee-2703caa74f9a/"
+                // }
+                ValidateIssuer = true,
+                //IssuerValidator = AadIssuerValidator.ValidateAadIssuer
+            };
             return View();
         }
     }
