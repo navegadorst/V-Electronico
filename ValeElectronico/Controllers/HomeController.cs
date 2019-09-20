60126ecdd5e2013c27fe712f9c9cda3d4b94c9e1
Alex Torres <alejandro.torres.e@gmail.com>
2019-09-20 03:01:14 -0500

    Integración de autenticación con Active Directory Azure

--- ValeElectronico/Controllers/HomeController.cs
+++ ValeElectronico/Controllers/HomeController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 
 namespace ValeElectronico.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
--- ValeElectronico/Controllers/LoginController.cs
+++ ValeElectronico/Controllers/LoginController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,6 +16,28 @@ namespace ValeElectronico.Controllers
         {
             return View("Login");
         }
-        
+        /// <summary>
+        /// Send an OpenID Connect sign-in request.
+        /// Alternatively, you can just decorate the SignIn method with the [Authorize] attribute
+        /// </summary>
+        public void SignIn()
+        {
+            if (!Request.IsAuthenticated)
+            {
+                HttpContext.GetOwinContext().Authentication.Challenge(
+                    new AuthenticationProperties { RedirectUri = "/" },
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType);
+            }
+        }
+
+        /// <summary>
+        /// Send an OpenID Connect sign-out request.
+        /// </summary>
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    CookieAuthenticationDefaults.AuthenticationType);
+        }
     }
 }
